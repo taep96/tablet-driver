a942e77966ffdcf2a23c2aa95a2591d7bd97fa7e
taep96 <64481039+taep96@users.noreply.github.com>
2022-01-30 17:31:47 +0100

    Some tweaks No unnecessary comments i16 -> u16 Print real pen coordinates instead of calculated ones

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,15 @@ use hidapi::HidApi;
 
 use crossterm::{cursor, execute, terminal};
 use fprint::fprint;
-use std::io::stdout;
 
 // CTL-472
 const VID: u16 = 0x056a;
 const PID: u16 = 0x037a;
 
 fn main() {
-    // Connect to device
     let api = HidApi::new().expect("HidApi: Failed to create API instance");
     let tablet = api.open(VID, PID).expect("HidApi: Failed to open tablet");
 
-    // Device info
     println!(
         "\
 Successfully connected to:
@@ -39,25 +36,23 @@ Successfully connected to:
             .expect("HidApi: Failed to get serial number"),
     );
 
-    let mut stdout = stdout();
+    let mut stdout = std::io::stdout();
 
     // TODO: buffer length = report length (returned by tablet.read)
     let mut buf = [0u8; 10];
 
     loop {
-        // Get data report
         tablet
             .read(&mut buf)
             .expect("HidApi: Failed to read to buffer");
 
-        // Format
+        // Make more readable | 255    255    ...
         let mut data_string = String::new();
 
         for u in &buf {
             data_string.push_str(&(u.to_string() + "\t"));
         }
 
-        // Clear last iterations output
         execute!(
             stdout,
             terminal::Clear(terminal::ClearType::CurrentLine),
@@ -66,29 +61,23 @@ Successfully connected to:
         )
         .expect("Crossterm: Failed to clear terminal");
 
-        // Raw usb data
+        // Parse pen coordinates
+        let (pen_x, pen_y) = (
+            u16::from_be_bytes([buf[3], buf[2]]) as f32,
+            u16::from_be_bytes([buf[5], buf[4]]) as f32,
+        );
+
+        // Convert pen coordinates
+        let (screen_x, screen_y) = (
+            ((pen_x - 2000.0) // - x_offset
+            / (15200.0 / 1920.0)) as u16,
+            ((pen_y - 2000.0) // - y_offset
+            / (9500.0 / 1080.0)) as u16,
+        );
+
         println!("{}", data_string);
 
-        // Parse pen coordinates
-        let (mut pen_x, mut pen_y) = (
-            i16::from_be_bytes([buf[3], buf[2]]) as f32,
-            i16::from_be_bytes([buf[5], buf[4]]) as f32,
-        );
-
-        // Area position/offset
-        pen_x -= 2000.0;
-        pen_y -= 2000.0;
-
-        // Remap to screen coordinates
-        let (screen_x, screen_y) = (
-            (pen_x / (15200.0 / 1920.0)) as i16,
-            (pen_y / (9500.0 / 1080.0)) as i16,
-        );
-
-        // Cursor coordinates on screen
         fprint!("{}x{}\t", screen_x, screen_y);
-
-        // Pen's coordinbates on tablet
         fprint!("{}x{}", pen_x, pen_y);
     }
 }
